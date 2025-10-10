97b21c43eb1431450d5eb048b9fb09c59a58a701
INTYSOFT <intisoft@outlook.com.pe>
2025-10-10 11:44:44 -0500

    Configu ag-grid

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,25 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
 import { appConfig } from 'app/app.config';
 
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+
+import { ApplicationConfig, LOCALE_ID, mergeApplicationConfig } from '@angular/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
+
+
+const esConfig: ApplicationConfig = {
+    providers: [
+        { provide: LOCALE_ID, useValue: 'es-ES' },
+        { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
+        
+    ],
+};
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
 );
