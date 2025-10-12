1e70429e431ba6a3df4b195098695f8312d308a2
INTYSOFT <intisoft@outlook.com.pe>
2025-10-12 00:50:48 -0500

    apo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -98,13 +98,7 @@ export const appRoutes: Route[] = [
                         loadChildren: () =>
                             import('app/modules/admin/mantenimiento/colegios/colegios.routes'),
                     },
-                    {
-                        path: 'apoderados',
-                        loadChildren: () =>
-                            import('app/modules/admin/mantenimiento/apoderados/apoderados.routes').then(
-                                (m) => m.apoderadosRoutes
-                            ),
-                    },
+                 
                     {
                         path: 'alumnos',
                         loadChildren: () =>
