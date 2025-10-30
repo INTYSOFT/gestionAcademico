655034a435fcfa3d8dcc544f5f92cd542dd3dd67
INTYSOFT <intisoft@outlook.com.pe>
2025-10-30 16:37:44 -0500

    feat(reportes): add evaluaciones programadas report

---
+++ src/app/modules/admin/reportes/evaluaciones-programadas/evaluaciones-programadas.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ReporteEvaluacionesProgramadasComponent } from './evaluaciones-programadas.component';
+
+export const reportesEvaluacionesProgramadasRoutes: Routes = [
+    {
+        path: '',
+        component: ReporteEvaluacionesProgramadasComponent,
+    },
+];
